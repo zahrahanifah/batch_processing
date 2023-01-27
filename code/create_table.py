13f81dc7db94e8fe0cc417a2b951f5afc73bb26f
Zahra_Hanifah <zahrahanifah072@gmail.com>
2023-01-27 21:48:06 +0700

    third commit

---
+++ code/create_table.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+#connect to postgresql
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=1234")
+cur = conn.cursor()
+
+#create table
+cur.execute("""
+                CREATE TABLE IF NOT EXISTS all_ticket(
+                    file_name text,
+                    date_insert date,
+                    title text,
+                    body text,
+                    created_on date,
+                    ticket_status text
+                )
+
+"""
+)
+conn.commit()
+
+print("Create Table Success")
---
+++ code/insert_table.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import glob
+import psycopg2
+from datetime import datetime
+
+#connect to postgresql
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=1234")
+cur = conn.cursor()
+
+#read file in directory
+path = os.getcwd()
+folder = glob.glob(os.path.join("E:/Zahra - Data Bootcamp/batch_processing/source", "*.csv"))
+
+#create query to insert multiple python variable in sql
+for file in folder:
+    df= pd.read_csv(file,sep=";")
+           
+    filename = str(file.split("\\")[-1])
+    current_time = datetime.now()
+
+    for index, row in df.iterrows():
+        cur.execute("INSERT INTO all_ticket VALUES (%s, %s, %s, %s, %s, %s)",(filename, current_time, row['title'], row['body'], row['created_on'], row['ticket_status']))
+    
+conn.commit()
+
+print("Create Table Success")
+
+
+
+
